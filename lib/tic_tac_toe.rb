9215497d4b2d462f63b134840b7e8962bcd4dd6f
Carrie Mathieu <carrieburks@gmail.com>
2020-08-08 03:06:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,7 +57,15 @@ def turn_count(board)
   return count
 end
 
+def current_player(board)
 
+  if turn_count(board) % 2 == 0 
+  if turn_count % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
 
 
 def play(board)
