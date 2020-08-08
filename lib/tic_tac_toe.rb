13097e1a0c2be4f7ac85c081d7dcfdd63a25b2bb
Carrie Mathieu <carrieburks@gmail.com>
2020-08-08 03:00:46 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,14 @@
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8], # bottom row
+  [0,3,6], # left column
+  [1,4,7], # middle column
+  [2,5,8], # right column
+  [0,4,8], # diagonal
+  [2,4,6] # diagonal
+]
+
 # display board according to board
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -12,16 +23,7 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [
-  [0,1,2], # Top row
-  [3,4,5],  # Middle row
-  [6,7,8], # bottom row
-  [0,3,6], # left column
-  [1,4,7], # middle column
-  [2,5,8], # right column
-  [0,4,8], # diagonal
-  [2,4,6] # diagonal
-]
+
 
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
