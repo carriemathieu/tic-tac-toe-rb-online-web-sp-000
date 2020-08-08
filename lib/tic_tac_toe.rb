56925cfaac2d535cb1116a8aea64650c4361d1b6
Carrie Mathieu <carrieburks@gmail.com>
2020-08-08 03:02:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,6 +23,42 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+# Define your play method below
+def play(board)
+  count = 0
+  until count == 9 
+    turn(board)
+    count += 1
+  end 
+end
 
 
 
