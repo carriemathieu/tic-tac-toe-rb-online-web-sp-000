98736dc77052feee800c0171bec7b9dc5f234f09
Carrie Mathieu <carrieburks@gmail.com>
2020-08-08 03:14:18 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,7 +40,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, "X" || "O")
+    move(board, index, value)
     display_board(board)
   else
     turn(board)
